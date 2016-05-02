dba6f274c5f77ea9267d835ad3762c219020dab8
Zhiyong Tan <yongish@gmail.com>
2016-05-01 18:02:10 -0700

    Auto-assign the default state to newly-created tickets

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 RSpec.feature "Users can create new tickets" do
-  let (:user) { FactoryGirl.create(:user) }
+  let!(:state) { FactoryGirl.create :state, name: "New", default: true }
+  let(:user) { FactoryGirl.create(:user) }
   before do
     login_as(user)
     project = FactoryGirl.create(:project, name: "Internet Explorer")
@@ -17,6 +18,7 @@ RSpec.feature "Users can create new tickets" do
     click_button "Create Ticket"
 
     expect(page).to have_content "Ticket has been created."
+    expect(page).to have_content "State: New"
     within("#ticket") do
       expect(page).to have_content "Author: #{user.email}"
     end
