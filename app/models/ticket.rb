7dc14328b7e2592af5b53e9436d9a405f02cf2d0
zhiyong tan <zhiyong@uber.com>
2016-05-28 22:49:40 -0700

    Added basic email ticket notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,11 +5,14 @@ class Ticket < ActiveRecord::Base
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_and_belongs_to_many :tags, uniq: true
+  has_and_belongs_to_many :watchers, join_table: "ticket_watchers",
+    class_name: "User", uniq: true
   attr_accessor :tag_names
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
   before_create :assign_default_state
+  after_create :author_watches_me
 
   searcher do
     label :tag, from: :tags, field: "name"
@@ -27,5 +30,11 @@ class Ticket < ActiveRecord::Base
 
     def assign_default_state
       self.state ||= State.default
+    end
+
+    def author_watches_me
+      if author.present? && !self.watchers.include?(author)
+        self.watchers << author
+      end
     end
 end
